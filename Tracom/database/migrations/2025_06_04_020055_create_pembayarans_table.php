feb1c1ba446727deb834aa7c839ccfb6045f4698
dvidkrnia <davidky1109@gmail.com>
2025-06-04 09:05:14 +0700

    migration

--- Tracom/database/migrations/2025_06_04_020055_create_pembayarans_table.php
+++ Tracom/database/migrations/2025_06_04_020055_create_pembayarans_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('pembayarans', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('menu');
+            $table->string('bukti');
             $table->timestamps();
         });
     }
